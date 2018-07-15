bfee4b12537a475e4ec8084e17721a52bc032790
Joachim Schwarm <joachim@schwarm.co>
2018-07-15 11:47:26 +0200

    "Solved" Extra Long Factorials

---
+++ algorithms/extra-long-factorials/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+
+	// your test-case goes here (split by \n)
+	in := "25"
+	out := "15511210043330985984000000"
+
+	input := bufio.NewReader(strings.NewReader(in))
+	output := new(bytes.Buffer)
+
+	solve(input, output)
+
+	if out != strings.Trim(output.String(), "\r\n") {
+		t.Fatal("\nexpected:\n", out, "\n\ngot:\n", output)
+	}
+}
