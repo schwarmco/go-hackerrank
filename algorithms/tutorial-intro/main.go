4f627ce0e42c39cbd922d7b278451d4dcbd747c7
Joachim Schwarm <joachim@schwarm.co>
2018-07-22 20:02:04 +0200

    Solved Tutorial Intro

---
+++ algorithms/tutorial-intro/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(reader io.Reader, writer io.Writer) {
+
+	V, _ := strconv.Atoi(readLine(reader))
+	readLine(reader)
+	arr := map[int]int{}
+
+	for idx, a := range strings.Split(readLine(reader), " ") {
+		i, _ := strconv.Atoi(a)
+		arr[i] = idx
+	}
+
+	fmt.Fprintln(writer, arr[V])
+}
+
+func main() {
+
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	solve(reader, writer)
+
+	writer.Flush()
+}
+
+func readLine(reader io.Reader) string {
+	r := bufio.NewReader(reader)
+	str, _, err := r.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
---
+++ algorithms/tutorial-intro/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+
+	// your test-case goes here (split by \n)
+	in := "4\n6\n1 4 5 7 9 12"
+	out := "1"
+
+	input := bufio.NewReader(strings.NewReader(in))
+	output := new(bytes.Buffer)
+
+	solve(input, output)
+
+	if out != strings.Trim(output.String(), "\r\n") {
+		t.Fatal("\nexpected:\n", out, "\n\ngot:\n", output)
+	}
+}
