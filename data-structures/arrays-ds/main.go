e17274b97877de1df5e0243de5671e172fdd1bc9
Joachim Schwarm <joachim@schwarm.co>
2018-07-22 21:21:16 +0200

    Solving Arrays - DS

---
+++ data-structures/arrays-ds/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(reader io.Reader, writer io.Writer) {
+
+	N, _ := strconv.Atoi(readLine(reader))
+	A := make([]string, N)
+
+	for _, s := range strings.Split(readLine(reader), " ") {
+		A[N-1] = s
+		N--
+	}
+
+	fmt.Fprintln(writer, strings.Join(A, " "))
+}
+
+func main() {
+
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	solve(reader, writer)
+
+	writer.Flush()
+}
+
+func readLine(reader io.Reader) string {
+	r := bufio.NewReader(reader)
+	str, _, err := r.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
---
+++ data-structures/arrays-ds/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+
+	// your test-case goes here (split by \n)
+	in := "4\n1 4 3 2"
+	out := "2 3 4 1"
+
+	input := bufio.NewReader(strings.NewReader(in))
+	output := new(bytes.Buffer)
+
+	solve(input, output)
+
+	if out != strings.Trim(output.String(), "\r\n") {
+		t.Fatal("\nexpected:\n", out, "\n\ngot:\n", output)
+	}
+}
