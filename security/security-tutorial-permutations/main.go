b7e25406d62d27049c5de34bf14816b0174937ac
Joachim Schwarm <joachim@schwarm.co>
2018-08-25 21:49:51 +0200

    Solved Security Permutations

---
+++ security/security-tutorial-permutations/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(reader io.Reader, writer io.Writer) {
+
+	n, _ := strconv.Atoi(readLine(reader))
+
+	X := make([]int, n+1)
+	for idx, x := range strings.Split(readLine(reader), " ") {
+		X[idx+1], _ = strconv.Atoi(x)
+	}
+
+	for i := 1; i <= n; i++ {
+		fmt.Fprintln(writer, X[X[i]])
+	}
+}
+
+func main() {
+
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	solve(reader, writer)
+
+	writer.Flush()
+}
+
+func readLine(reader io.Reader) string {
+	r := bufio.NewReader(reader)
+	str, _, err := r.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
