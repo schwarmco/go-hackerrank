f7efa48234483157bcf768b1ecf75a976bb7d0c1
Joachim Schwarm <joachim@schwarm.co>
2018-08-25 20:14:49 +0200

    Solving "Security > Tutorial Functions"

---
+++ security/security-tutorial-functions/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(reader io.Reader, writer io.Writer) {
+	x, _ := strconv.Atoi(readLine(reader))
+	fmt.Fprintf(writer, "%d\n", x%11)
+}
+
+func main() {
+
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	solve(reader, writer)
+
+	writer.Flush()
+}
+
+func readLine(reader io.Reader) string {
+	r := bufio.NewReader(reader)
+	str, _, err := r.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
